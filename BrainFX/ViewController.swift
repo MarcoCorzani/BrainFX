5361bda40d1af9c1186993459d66f7fe123b83b7
Marco <Marco@Marcos-iMac.fritz.box>
2014-11-04 19:03:18 +0100

    Alles neu

--- BrainFX/ViewController.swift
+++ BrainFX/ViewController.swift
@@ -7,19 +7,166 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var people = [NSManagedObject]()
+    
+    @IBAction func addSpruch(sender: AnyObject) {
+        var alert = UIAlertController(title: "New name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.saveName(textField.text)
+                self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "\"Meine Brainers\""
+        tableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
+    }
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return people.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+            
+            let person = people[indexPath.row]
+            cell.textLabel.text = person.valueForKey("adjektiv") as String?
+            
+            return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func saveName(name: String) {
+        //1
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        //2
+        let entity =  NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        
+        
+        //3
+        person.setValue(name, forKey: "adjektiv")
+        
+        //4
+        var error: NSError?
+        
+            if !managedContext.save(&error) {println("Could not save \(error) , \(error?.userInfo)")
+        }  
+        //5
+        people.append(person)
+    }
+    
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName:"Person")
+        
+        //3
+        var error: NSError?
+        
+        let fetchedResults = managedContext.executeFetchRequest(fetchRequest, error: &error) as [NSManagedObject]?
+        
+        if let results = fetchedResults {
+            people = results
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
     }
 
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+            return true
+       }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+//            if editingStyle == UITableViewCellEditingStyle.Delete {
+//            
+//            people.removeAtIndex(indexPath.row)
+//
+//            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+//            
+//            self.tableView.reloadData()
+//            
+//        }
+    
+            let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+            let managedContext = appDelegate.managedObjectContext!
+                //Das ist swipe to delete
+            if editingStyle == UITableViewCellEditingStyle.Delete {
+                //Das entfernt entgültig aus CoreDate
+                managedContext.deleteObject(people[indexPath.row] as NSManagedObject)
+               //Das entfernt es aus dem Table
+                people.removeAtIndex(indexPath.row)
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+
+    
+    
 
 }
+
+
+
+
+
 
